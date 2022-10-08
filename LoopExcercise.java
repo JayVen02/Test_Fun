c067a04df6ec08e1c4105313ddd106f556562932
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2022-10-08 23:58:42 +0800

    Add files via upload

---
+++ LoopExcercise.java
@@ -0,0 +1,15 @@
+
+public class LoopExcercise {
+
+	public static void main(String[] args) {
+		int i;      //loopval
+		int N = 11; //valueend
+		int a = 0;  //Addition Value
+		   
+		for(/* loopval (i) */i = 1;/* LoopVal (i) */ i < /* ValueEnd (N) */ N;/* loopVal (increment++) */ i++) {
+			/* Addition --> */ a = a + i;
+		}
+		System.out.println("Total: " + a );
+	}
+
+}
